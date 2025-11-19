bb0a25b006ae12159536359f7c9c8fc95e42618e
tanghubin <1064766891@qq.com>
2025-11-19 10:25:44 +0800

    feat: setting文件设置

--- matrix-frontend/eslint.config.js
+++ matrix-frontend/eslint.config.js
@@ -1,21 +1,21 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
-import prettier from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier';
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   prettierConfig,
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
@@ -27,8 +27,8 @@ export default defineConfig([
     plugins: {
       prettier,
     },
-  //  rules: {
-  //     'prettier/prettier': 'error',  // Prettier 错误显示为 ESLint 错误
-  //   },
-  }
-])
+    rules: {
+      "prettier/prettier": "error", // Prettier 错误显示为 ESLint 错误
+    },
+  },
+]);
